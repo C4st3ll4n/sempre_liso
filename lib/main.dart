e2ed06151afb33d49d659eed317f22fdad279292
Pedro Henrique <p13dr0h@gmail.com>
2020-03-09 19:49:09 -0300

    setup do projeto feito para o curso de Flutter do SENAC

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:sempreliso/screen/HomePage.dart';
+
+void main() => runApp(MaterialApp(
+  home: HomePage(),
+));
---
+++ lib/screen/HomePage.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    List<Financas> financeiro = [
+      Financas(8, "Vendeu app", 600, false),
+      Financas(10, "Vendeu mouse", 80.0, false),
+      Financas(11, "Compra de Teclado", 175.0, true),
+      Financas(12, "Compra de MacBook", 5000.0, true),
+      Financas(16, "Compra de MacBook", 5000.0, true),
+      Financas(15, "Compra de MacBook", 5000.0, true),
+      Financas(14, "Compra de MacBook", 5000.0, true),
+      Financas(13, "Compra de MacBook", 5000.0, true),
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Sempre liso"),
+          centerTitle: true,
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.add),
+        ),
+        body: SafeArea(
+          child: ListaLancamento(financeiro: financeiro,),
+        ),
+      ),
+    );
+  }
+}
+
+class Financas {
+  int id;
+  String titulo;
+  double valor;
+  bool isDebito;
+
+  Financas(this.id, this.titulo, this.valor, this.isDebito);
+}
+
+class ListaLancamento extends StatelessWidget {
+  final List<Financas> financeiro;
+
+  const ListaLancamento({Key key, this.financeiro}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        scrollDirection: Axis.vertical,
+        itemCount: financeiro.length,
+        itemBuilder: (ctx, index) {
+          Financas fin = financeiro[index];
+          return ItemLancamento(financa: fin,);
+        });
+  }
+}
+
+class ItemLancamento extends StatelessWidget {
+  final Financas financa;
+
+  const ItemLancamento({Key key, this.financa}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: Text(financa.titulo),
+        subtitle: Text("R\$${financa.valor}"),
+        leading: financa.isDebito
+            ? Icon(Icons.trending_down, color: Colors.red)
+            : Icon(Icons.trending_up,color: Colors.blue),
+      ),
+    );
+  }
+}
+
+
